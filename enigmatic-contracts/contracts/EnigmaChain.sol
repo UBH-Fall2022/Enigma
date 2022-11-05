fc900531c81ab6aa83667f96e820c2502b72224a
Ankith Bala <bala.ankith@gmail.com>
2022-11-05 19:06:59 -0400

    feature/3-return-all-contributors

--- enigmatic-contracts/contracts/EnigmaChain.sol
+++ enigmatic-contracts/contracts/EnigmaChain.sol
@@ -8,22 +8,51 @@ pragma solidity ^0.8.13;
 // coin/token contracts.
 
 contract EnigmaChain {
+    struct ContributorStruct {
+        address contributor;
+        uint256 weight;
+        //address[] contributors;
+        //mapping(address => uint256) myMappingInStruct;
+
+    }
+
+    uint sizeOfContributors;
+
     mapping(address => uint256) contributors;
     address owner;
-    address[] verrifiers;
+    address[] verifiers;
+
+    uint addressRegistryCount;   
+
+    ContributorStruct[] AllContributors;
+    
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     constructor() {
         owner = tx.origin;
-        verrifiers = [tx.origin];
+        verifiers = [tx.origin];
+        
     }
+    /*
+    function make() {
+        //If tx sender is owner
+        // Add value to verifiers
+    }
+    */
+
 
     function addContrib(address contributor) public returns (bool) {
-        for (uint256 i = 0; i < verrifiers.length; i++) {
-            if (verrifiers[i] == tx.origin) {
+        for (uint256 i = 0; i < verifiers.length; i++) {
+            if (verifiers[i] == tx.origin) {
                 contributors[contributor] += 1;
                 contributors[tx.origin] += 1;
+
+                
+                AllContributors[i].contributor = contributor;
+                AllContributors[i].weight +=1;
+                sizeOfContributors++;
+
                 return true;
             }
         }
@@ -39,6 +68,27 @@ contract EnigmaChain {
         contributors[receiver] += amount;
         emit Transfer(msg.sender, receiver, amount);
         return true;
+    }
+/*
+ function getAll() public view returns (address[] memory){
+    address[] memory ret = new address[](addressRegistryCount);
+    for (uint i = 0; i < addressRegistryCount; i++) {
+        ret[i] = contributors;
+    }
+    return ret;
+}
+*/
+
+    function getAllContributors() public view returns (ContributorStruct[] memory) {
+    return AllContributors;
+    }
+
+    function sendViaCall(address receiver) public payable {
+
+        //uint value = contributors[receiver];
+        
+        (bool sent, bytes memory data) = receiver.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
     }
 
     function getBalance(address addr) public view returns (uint256) {
